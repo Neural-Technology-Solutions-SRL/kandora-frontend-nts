73730f0fbc97750196ebd08f81f18968cb8f1521
kandora santana <110423707+ksantana-nts@users.noreply.github.com>
2022-08-10 10:03:33 -0400

    new-functionBased
    code update before uploading to hosting service(pages)

--- TodoContainer.js
+++ TodoContainer.js
@@ -1,12 +1,101 @@
-import React from "react"
-class TodoContainer extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </div>
+import React, { useState, useEffect } from "react"
+import Header from "./Header"
+import InputTodo from "./InputTodo"
+import TodosList from "./TodosList"
+import { v4 as uuidv4 } from "uuid"
+import { Route, Routes } from "react-router-dom" 
+import About from "../pages/About"
+import NotMatch from "../pages/NotMatch"
+import NavBar from "./NavBar"
+
+
+
+const TodoContainer = () => {
+  const [todos, setTodos] = useState(getInitialTodos())
+
+  const handleChange = id => {
+    setTodos(prevState =>
+      prevState.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo
+      })
     )
   }
+
+  const delTodo = id => {
+    setTodos([
+      ...todos.filter(todo => {
+        return todo.id !== id
+      }),
+    ])
+  }
+
+  const addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    }
+    setTodos([...todos, newTodo])
+  }
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      })
+    )
+  }
+
+  useEffect(() => {
+    console.log("test run")
+
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+
+    if (loadedTodos) {
+      setTodos(loadedTodos)
+    }
+  }, [setTodos])
+
+  function getInitialTodos() {
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const savedTodos = JSON.parse(temp)
+    return savedTodos || []
+  }
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/"  element={
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={addTodoItem} />
+              <TodosList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
+          </div>
+        }/>
+        <Route path="about/*" element={ <About /> } />
+        <Route path="*" element={ <NotMatch /> } />
+      </Routes>
+    </>
+  )
 }
 export default TodoContainer
